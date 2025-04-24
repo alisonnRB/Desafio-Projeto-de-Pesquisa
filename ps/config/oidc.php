b5df9047da326735cdb37f2fb8cd255746c485f8
alisonnRB <alisonrbjs@gmail.com>
2025-04-24 14:38:20 -0300

    .env enviroments logic move to laravels config

---
+++ ps/config/oidc.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'keycloak_url' => env('KEYCLOAK_URL'),
+    'keycloak_external_url' => env('KEYCLOAK_EXTERNAL_URL'),
+    'realm' => env('KEYCLOAK_REALM'),
+    'client_id' => env('KEYCLOAK_CLIENT_ID'),
+    'client_secret' => env('KEYCLOAK_CLIENT_SECRET'),
+    'client_name' => env('CLIENT_NAME'),
+];
